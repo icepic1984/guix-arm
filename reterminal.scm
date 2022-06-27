33861ceedff066ebdbc48366fc84cd0c1f1a0212
Шляк Павел Вячеславович <p.shlyak@servplus.ru>
2022-06-27 15:35:02 +0300

    Add bq24179_charger driver

---
+++ reterminal.scm
@@ -0,0 +1,34 @@
+(use-modules (guix packages))
+(use-modules (guix download))
+(use-modules (guix git-download))
+(use-modules (guix gexp))
+(use-modules ((guix licenses) #:prefix license:))
+(use-modules (guix build-system linux-module))
+
+(include "rpi-kernel.scm")
+
+(define-public bq24179-charger-linux-module
+  (package
+    (name "bq24179-charger-linux-module")
+    (version "0.1")
+    (source
+     (file-append (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/shlyakpavel/seeed-linux-dtoverlays-guix.git")
+             (commit "589dab165f7a55eec0cc5fa25cc0bf892f4aa52c")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "002y8x0dmglhfgm60az6059jjnfm5q1zxdfp0b4s8dqybhjbdhb5")))
+		 "/modules/bq24179_charger")
+		)
+    (build-system linux-module-build-system)
+    (arguments
+     (list #:tests? #f #:linux linux-raspberry-5.10))                ; no test suite, RPI Linux
+    (home-page "https://github.com/Seeed-Studio/seeed-linux-dtoverlays/tree/master/modules/bq24179_charger")
+    (synopsis "Linux kernel module for bq24179_charger found in Seed Studio ReTerminal")
+    (description
+     "This is the Linux kernel bq24179_charger driver")
+    (license license:gpl2)))
+
+bq24179-charger-linux-module
