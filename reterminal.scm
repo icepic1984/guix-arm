190e9fcf7a7ac6112238210144388fec5baa1c5f
Шляк Павел Вячеславович <p.shlyak@servplus.ru>
2022-06-27 15:53:54 +0300

    MIPI DSI driver & upstream links

--- reterminal.scm
+++ reterminal.scm
@@ -15,7 +15,7 @@
      (file-append (origin
        (method git-fetch)
        (uri (git-reference
-             (url "https://github.com/shlyakpavel/seeed-linux-dtoverlays-guix.git")
+             (url "https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git")
              (commit "589dab165f7a55eec0cc5fa25cc0bf892f4aa52c")))
        (file-name (git-file-name name version))
        (sha256
@@ -39,7 +39,7 @@
      (file-append (origin
        (method git-fetch)
        (uri (git-reference
-             (url "https://github.com/shlyakpavel/seeed-linux-dtoverlays-guix.git")
+             (url "https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git")
              (commit "589dab165f7a55eec0cc5fa25cc0bf892f4aa52c")))
        (file-name (git-file-name name version))
        (sha256
@@ -49,7 +49,7 @@
     (build-system linux-module-build-system)
     (arguments
      (list #:tests? #f #:linux linux-raspberry-5.10))                ; no test suite, RPI Linux
-    (home-page "https://github.com/shlyakpavel/seeed-linux-dtoverlays-guix/tree/master/modules/lis3lv02d")
+    (home-page "https://github.com/Seeed-Studio/seeed-linux-dtoverlays/tree/master/modules/lis3lv02d")
     (synopsis "Linux kernel module for GROVE 3-Axis Digital Accelerometer found in Seed Studio ReTerminal")
     (description
      "This is the Linux kernel GROVE 3-Axis Digital Accelerometer driver")
@@ -63,7 +63,7 @@
      (file-append (origin
        (method git-fetch)
        (uri (git-reference
-             (url "https://github.com/shlyakpavel/seeed-linux-dtoverlays-guix.git")
+             (url "https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git")
              (commit "589dab165f7a55eec0cc5fa25cc0bf892f4aa52c")))
        (file-name (git-file-name name version))
        (sha256
@@ -73,10 +73,34 @@
     (build-system linux-module-build-system)
     (arguments
      (list #:tests? #f #:linux linux-raspberry-5.10))                ; no test suite, RPI Linux
-    (home-page "https://github.com/shlyakpavel/seeed-linux-dtoverlays-guix/tree/master/modules/ltr30x")
+    (home-page "https://github.com/Seeed-Studio/seeed-linux-dtoverlays/tree/master/modules/ltr30x")
     (synopsis "Linux kernel module for ltr30x ambient light and proximity sensor found in Seed Studio ReTerminal")
     (description
      "This is the Linux kernel ltr30x ambient light and proximity sensor driver")
     (license license:gpl2)))
 
-ltr30x-linux-module
+(define-public mipi_dsi-linux-module
+  (package
+    (name "mipi_dsi-linux-module")
+    (version "0.1")
+    (source
+     (file-append (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git")
+             (commit "589dab165f7a55eec0cc5fa25cc0bf892f4aa52c")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "002y8x0dmglhfgm60az6059jjnfm5q1zxdfp0b4s8dqybhjbdhb5")))
+		 "/modules/mipi_dsi")
+		)
+    (build-system linux-module-build-system)
+    (arguments
+     (list #:tests? #f #:linux linux-raspberry-5.10))                ; no test suite, RPI Linux
+    (home-page "https://github.com/Seeed-Studio/seeed-linux-dtoverlays/tree/master/modules/mipi_dsi")
+    (synopsis "Linux kernel module for MIPI-DSI panel with touch panel attached to I2C bus found in Seed Studio ReTerminal")
+    (description
+     "This is a linux kernel driver for MIPI-DSI panel with touch panel attached to I2C bus.")
+    (license license:gpl2)))
+
+mipi_dsi-linux-module
