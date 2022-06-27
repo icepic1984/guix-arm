bb23460480611d2a30222904efe2d15d05570e72
Шляк Павел Вячеславович <p.shlyak@servplus.ru>
2022-06-27 15:45:18 +0300

    ltr30x ALSPS

--- reterminal.scm
+++ reterminal.scm
@@ -49,10 +49,34 @@
     (build-system linux-module-build-system)
     (arguments
      (list #:tests? #f #:linux linux-raspberry-5.10))                ; no test suite, RPI Linux
-    (home-page "https://github.com/Seeed-Studio/seeed-linux-dtoverlays/tree/master/modules/bq24179_charger")
+    (home-page "https://github.com/shlyakpavel/seeed-linux-dtoverlays-guix/tree/master/modules/lis3lv02d")
     (synopsis "Linux kernel module for GROVE 3-Axis Digital Accelerometer found in Seed Studio ReTerminal")
     (description
      "This is the Linux kernel GROVE 3-Axis Digital Accelerometer driver")
     (license license:gpl2)))
 
-lis3lv02d-linux-module
+(define-public ltr30x-linux-module
+  (package
+    (name "ltr30x-linux-module")
+    (version "0.1")
+    (source
+     (file-append (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/shlyakpavel/seeed-linux-dtoverlays-guix.git")
+             (commit "589dab165f7a55eec0cc5fa25cc0bf892f4aa52c")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "002y8x0dmglhfgm60az6059jjnfm5q1zxdfp0b4s8dqybhjbdhb5")))
+		 "/modules/ltr30x")
+		)
+    (build-system linux-module-build-system)
+    (arguments
+     (list #:tests? #f #:linux linux-raspberry-5.10))                ; no test suite, RPI Linux
+    (home-page "https://github.com/shlyakpavel/seeed-linux-dtoverlays-guix/tree/master/modules/ltr30x")
+    (synopsis "Linux kernel module for ltr30x ambient light and proximity sensor found in Seed Studio ReTerminal")
+    (description
+     "This is the Linux kernel ltr30x ambient light and proximity sensor driver")
+    (license license:gpl2)))
+
+ltr30x-linux-module
