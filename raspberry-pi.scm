302ddcd17f9fade09beaa9f92b083be3b03fae90
Шляк Павел Вячеславович <p.shlyak@servplus.ru>
2022-07-03 07:51:43 +0300

    Try to revive USB

--- raspberry-pi.scm
+++ raspberry-pi.scm
@@ -73,8 +73,8 @@ load the Grub bootloader located in the 'Guix_image' root partition."
 		(targets '("/dev/vda"))))
    (initrd-modules '())
    (kernel linux-raspberry-5.10)
-   (kernel-arguments (cons* "console=serial0,9600" "console=ttyAMA0,9600" "8250.nr_uarts=1" "kgdboc=serial0,9600" "module_blacklist=vc4"
-			  %default-kernel-arguments))
+   ;;(kernel-arguments (cons* "console=serial0,9600" "console=ttyAMA0,9600" "8250.nr_uarts=1" "kgdboc=serial0,9600" "module_blacklist=vc4"
+	 ;;		  %default-kernel-arguments))
    (firmware (list raspberrypi-firmware))
    (file-systems (append (list 
                           (file-system
